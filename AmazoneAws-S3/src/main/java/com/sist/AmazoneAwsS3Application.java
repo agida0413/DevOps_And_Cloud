3f9f86b17fc624f410d687a50b86bbf6004c52fa
agida0413 <144654789+agida0413@users.noreply.github.com>
2024-08-11 14:24:45 +0900

    fileupload

--- AmazoneAws-S3/src/main/java/com/sist/AmazoneAwsS3Application.java
+++ AmazoneAws-S3/src/main/java/com/sist/AmazoneAwsS3Application.java
@@ -8,6 +8,7 @@ public class AmazoneAwsS3Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AmazoneAwsS3Application.class, args);
+		
 	}
 
 }
---
+++ AmazoneAws-S3/src/main/java/com/sist/config/S3Config.java
@@ -0,0 +1,32 @@
+package com.sist.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.S3ClientBuilder;
+
+@Configuration
+public class S3Config {
+
+    @Value("${s3.credentials.access-key}")
+    private String accessKey;
+
+    @Value("${s3.credentials.secret-key}")
+    private String secretKey;
+
+    @Value("${s3.credentials.region}")
+    private String region;
+
+    @Bean
+    public S3Client s3Client() {
+        AwsBasicCredentials awsCredentials = AwsBasicCredentials.create(accessKey, secretKey);
+        return S3Client.builder()
+                .region(Region.of(region))
+                .credentialsProvider(StaticCredentialsProvider.create(awsCredentials))
+                .build();
+    }
+}
